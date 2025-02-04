888a9ea67c6ba8a9dbe9bac74069774fd41b9a37
blazecordero1 <blazecordero1@gmail.com>
2025-02-04 21:32:18 +0800

    create student func

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Student } from './student/student.entity';
+import { StudentModule } from './student/student.module';
+import { StudentModule } from './student/student.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root', 
+      password: 'password', 
+      database: 'student_db',
+      entities: [Student],
+      synchronize: true,
+    }),
+    StudentModule,
+  ],
+})
+export class AppModule {}
